54919b915fe1a15e564862d9ae350ad6408d4e14
retula <158176293+retula@users.noreply.github.com>
2025-01-26 14:21:10 +0200

    added sfx

--- Assets/Scripts/BubbleScript.cs
+++ Assets/Scripts/BubbleScript.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class BubbleScript : FallingObject
 {
+    // SoundEffectsPlayer sfxScript;
+    // [SerializeField] AudioClip bubble; 
+
     [SerializeField] Sprite[] sprites;
     SpriteRenderer spriteRenderer;
     void Awake()
     {
+        // sfxScript = GameObject.FindGameObjectWithTag("audioManager").GetComponent<SoundEffectsPlayer>();
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         spriteRenderer.sprite = sprites[Random.Range(0, sprites.Length)];
         speed = Random.Range(speed - 50, speed + 40);
@@ -14,6 +18,8 @@ public class BubbleScript : FallingObject
     {
         if (collision.tag == "Player")
         {
+            // sfxScript.playSound(bubble);
+
             PlayerHealth.Instance.increaseHealth();
             Destroy(gameObject);
 
--- Assets/Scripts/CollectibleScript.cs
+++ Assets/Scripts/CollectibleScript.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class CollectibleScript : FallingObject
 {
-    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
